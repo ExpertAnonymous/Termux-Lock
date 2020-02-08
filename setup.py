d0eb76a428d864c2b6db02d80af811690cdeeb8c
ExpertAnonymous <60571839+ExpertAnonymous@users.noreply.github.com>
2020-02-08 07:19:15 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Setup and activation of Termux-Lock
+
+import os,sys
+
+os.chdir('/data/data/com.termux/files/usr/etc')
+
+opr = open('bash.bashrc','a')
+opr.write("\nalias txl='python /data/data/com.termux/files/home/Termux-Lock/Termux-Lock.py -l'\n")
+opr.write('txl\n')
+opr.close()
+
+os.system('apt install figlet')
+os.system('pip install stdiomask')
+os.system('pip install lolcat')
